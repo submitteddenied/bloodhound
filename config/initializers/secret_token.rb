a316c710b72d9d3aaa04f55fd5b9c54784f236cd
Michael Jensen <engineers+mjensen@sharethrough.com>
2012-07-15 16:52:54 -0700

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloodhound::Application.config.secret_token = 'db0dc9f8f06357998e80cb9fabecc6ac9c00edaa59df2a9543c03e45b90f8676de939f2eb0e59265d2c1e5bf69c945d142661fe5a919dc737696e6d78988e713'
