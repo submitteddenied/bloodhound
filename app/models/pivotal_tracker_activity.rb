91ee067ab2a1080d6032cc8782291556d889eee6
Michael Jensen <engineers+mjensen@sharethrough.com>
2012-07-22 21:05:12 -0700

    Remove XML parsing stuff

--- app/models/pivotal_tracker_activity.rb
+++ app/models/pivotal_tracker_activity.rb
@@ -1,25 +1,6 @@
 class PivotalTrackerActivity < ActiveRecord::Base
   attr_accessible :pt_id, :version, :event_type, :occurred_at, :author, :project_id, :description, :project_id, :story_id, :story_url
 
-  def self.from_xml(xml)
-    activities = []
-
-    doc = Nokogiri.XML(xml)
-    doc.xpath('//stories/story').each do |story|
-      activities << self.create(
-        author: doc.xpath("//activity/author").first.text(),
-        pt_id: doc.xpath("//activity/project_id").first.text(),
-        version: doc.xpath("//activity/version").first.text(),
-        event_type: doc.xpath("//activity/event_type").first.text(),
-        occurred_at: Time.parse(doc.xpath("//activity/occurred_at").first.text()),
-        description: doc.xpath("//activity/description").first.text(),
-        story_id: story.xpath('.//id').first.text(),
-        story_url: story.xpath('.//url').first.text()
-      )
-    end
-    activities
-  end
-
   def self.from_api(data)
     activities = []
     data = data['activity']
--- spec/models/pivotal_tracker_activity_spec.rb
+++ spec/models/pivotal_tracker_activity_spec.rb
@@ -5,34 +5,6 @@ describe PivotalTrackerActivity do
     PivotalTrackerActivity.new.should be_valid
   end
 
-  describe '.from_xml' do
-    let(:start_story_xml) { File.read('spec/fixtures/api_requests/pivotal_tracker/start_story.xml') } 
-    let(:label_two_stories) { File.read('spec/fixtures/api_requests/pivotal_tracker/label_two_stories.xml') } 
-
-    it 'converts xml for starting a single story into an instance of PivotalTrackerActivity' do
-      activities = PivotalTrackerActivity.from_xml(start_story_xml)
-
-      activity = activities.first
-
-      activity.author.should == 'Michael Jensen'
-      activity.pt_id.should == 518057
-      activity.version.should == 116
-      activity.event_type.should == 'story_update'
-      activity.occurred_at.should == Time.parse('2012/07/16 00:30:00 UTC')
-      activity.description.should == 'Michael Jensen started "Move existing data to the Database"'
-      activity.story_id.should == 28203109 
-      activity.story_url.should == 'http://www.pivotaltracker.com/services/v3/projects/518057/stories/28203109'
-    end
-
-    it 'converts xml for labeling of two stories into two instances of PivotalTrackerActivity' do
-      activities = PivotalTrackerActivity.from_xml(label_two_stories)
-
-      activities.count.should == 2
-
-      activities[0].author.should == 'Michael Jensen'
-    end
-  end
-  
   describe '.from_api' do
     let(:api_data) do
       {
