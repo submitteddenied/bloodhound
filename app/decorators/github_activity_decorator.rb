93740afca89d2eb8618da3c0d2e82945f47667eb
Wayne Mak <makwayne@gmail.com>
2012-08-18 09:39:58 -0700

    Add github logo and username to the stream

--- app/decorators/github_activity_decorator.rb
+++ app/decorators/github_activity_decorator.rb
@@ -6,4 +6,12 @@ class GithubActivityDecorator
   def partial
     'github_activity'
   end
+
+  def author
+    @activity.author_name
+  end
+
+  def occurred_at
+    @activity.occurred_at
+  end
 end
