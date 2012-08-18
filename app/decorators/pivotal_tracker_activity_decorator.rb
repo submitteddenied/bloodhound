3bca6f0496fb11403c2e75b550975d6f4348c8c5
Wayne Mak <makwayne@gmail.com>
2012-08-17 23:39:12 -0700

    Getting some style in there

--- app/decorators/pivotal_tracker_activity_decorator.rb
+++ app/decorators/pivotal_tracker_activity_decorator.rb
@@ -1,4 +1,12 @@
 class PivotalTrackerActivityDecorator
+  STORY_EVENTS = [
+    'finished',
+    'started',
+    'estimated',
+    'edited',
+    'accepted',
+  ]
+
   def initialize(activity)
     @activity = activity
   end
@@ -7,7 +15,9 @@ class PivotalTrackerActivityDecorator
     'pivotal_tracker_activity'
   end
 
-  def event_type
-    @activity.event_type
+  def event_tag
+    r = %r|^#{@activity.author} ([a-zA-Z]*) |
+    m = r.match @activity.description
+    m[1] if STORY_EVENTS.include?(m[1])
   end
 end
