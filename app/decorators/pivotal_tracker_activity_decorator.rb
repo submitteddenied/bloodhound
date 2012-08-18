2668391a6b03132ff0c8fbf7a70ceee5f79cb53f
Wayne Mak <makwayne@gmail.com>
2012-08-18 01:46:24 -0700

    Style pivotal tracker activity

--- app/decorators/pivotal_tracker_activity_decorator.rb
+++ app/decorators/pivotal_tracker_activity_decorator.rb
@@ -20,4 +20,18 @@ class PivotalTrackerActivityDecorator
     m = r.match @activity.description
     m[1] if STORY_EVENTS.include?(m[1])
   end
+
+  def author
+    @activity.author
+  end
+
+  def story_title
+    r = %r|^#{@activity.author} [a-zA-Z]* "(.*)"|
+    m = r.match @activity.description
+    m[1]
+  end
+
+  def occurred_at
+    @activity.occurred_at
+  end
 end
