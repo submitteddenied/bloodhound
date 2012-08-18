1a8097a6c15b46352c0281c2db433a68b093d56a
Wayne Mak <makwayne@gmail.com>
2012-08-18 09:05:41 -0700

    Simplify the tracker logic

--- app/decorators/pivotal_tracker_activity_decorator.rb
+++ app/decorators/pivotal_tracker_activity_decorator.rb
@@ -1,29 +1,16 @@
 class PivotalTrackerActivityDecorator
-  STORY_EVENTS = [
-    'finished',
-    'started',
-    'estimated',
-    'edited',
-    'accepted',
-    'rejected',
-    'delivered',
-    'added comment'
-  ]
-
   def initialize(activity)
     @activity = activity
   end
 
   def partial
-    return 'pivotal_tracker_activity_story_update'
+    'pivotal_tracker_activity_story_update'
   end
 
   def event_tag
-    r = %r|^#{@activity.author} (.*)"|
+    r = %r{^#{@activity.author} (.*?)(:|")}
     m = r.match @activity.description
-    STORY_EVENTS.each do |event|
-      return event if m[1] =~ %r{^#{event}}
-    end
+    m[1].strip
   end
 
   def author
